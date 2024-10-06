3e49560894d8afb43fc3af5aea93cced8a3fea76
Jolly Zheng <jz767@cornell.edu>
2024-10-06 15:23:20 -0400

    comment changes on filter

--- filter.py
+++ filter.py
@@ -1,17 +1,10 @@
 import pandas as pd
 
-# Load the CSV file (use the correct delimiter)
 file_path = 'bestSongs.csv'
 df = pd.read_csv(file_path, delimiter=';')
 
-# Filter out songs from before the year 2000
 df_filtered = df[df['year'] >= 2000]
-
-# Sort by 'year' and 'popularity', descending order
 df_sorted = df_filtered.sort_values(['year', 'popularity'], ascending=[True, False])
-
-# Group by 'year' and take the top 15 songs of each year
 top_songs = df_sorted.groupby('year').head(15)
 
-# Save the result to a new CSV file
 top_songs.to_csv('top_15_songs_per_year.csv', index=False)
